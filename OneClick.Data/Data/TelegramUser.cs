5be1a33f795559e7b909c926a1a31b2d17f91bc2
Роман Смирнов <magicom@yandex.ru>
2024-10-01 17:28:52 +0300

    chp

--- OneClick.Data/Data/TelegramUser.cs
+++ OneClick.Data/Data/TelegramUser.cs
@@ -5,6 +5,7 @@
         public long Id { get; set; }
         public string? Name { get; set; }
         public string? OneClickLogin { get; set; }
+        public string? OneClickUserId { get; set; }
         public string? Language { get; set; }
         public long TelegramId { get; set; }
         public DateTime Registered { get; set; }
